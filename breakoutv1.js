fe4c378e4a4469640b0a0fab7d2ad36f9decf1bc
Fernando Delgado <fernando.delgado@admgroup.com>
2023-09-15 14:38:44 +0200

    Code for the Part III of the tutorial

--- breakoutv1.js
+++ breakoutv1.js
@@ -7,12 +7,32 @@ $(document).ready(function () {
     var WIDTH; // Width of the frame
     var HEIGHT; // Height of the frame
     var ctx;
+    var paddlex;
+    var paddleh;
+    var paddlew;
+    var rightDown = false;
+    var leftDown = false;
+    var canvasMinX;
+    var canvasMaxX;
+
+    $(document).keydown(onKeyDown);
+    $(document).keyup(onKeyUp);
+    $(document).mousemove(onMouseMove);
 
     function init() {
         ctx = $('#canvas')[0].getContext("2d"); // Obtain the context from the canvas
         WIDTH = $("#canvas").width(); // Assign the size to the variables
         HEIGHT = $("#canvas").height();
+        canvasMinX = $("#canvas").offset().left;
+        canvasMaxX = canvasMinX + WIDTH;
+        init_paddle();
         return setInterval(paint, 10); // Call paint() every 10 ms
+    }
+
+    function init_paddle() {
+        paddlex = WIDTH / 2;
+        paddleh = 10;
+        paddlew = 75;
     }
 
     // Draw a circle with its center on (x,y), radius r and a color
@@ -43,18 +63,49 @@ $(document).ready(function () {
         ctx.strokeRect(0, 0, WIDTH, HEIGHT);
     }
 
+    function onKeyDown(evt) {
+        if (evt.keyCode == 39) rightDown = true;
+        else if (evt.keyCode == 37) leftDown = true;
+    }
+
+    function onKeyUp(evt) {
+        if (evt.keyCode == 39) rightDown = false;
+        else if (evt.keyCode == 37) leftDown = false;
+    }
+
+    // Move the paddle if the mouse is inside the canvas:
+function onMouseMove(evt) {
+    if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
+    paddlex = evt.pageX - canvasMinX;
+    }
+   }
+    
     function paint() {
         clear();
         circle(x, y, 10, "black");
 
-        if (x + dx > WIDTH || x + dx < 0)
-        dx = -dx;
-        if (y + dy > HEIGHT || y + dy < 0)
-        dy = -dy;
+        // Move the paddle if left or right are pressed
+        if (rightDown) paddlex += 5;
+        else if (leftDown) paddlex -= 5;
+
+        // Draw the paddle
+        rect(paddlex, HEIGHT - paddleh, paddlew, paddleh);
+
+        // Check boundaries to bounce
+        if (x + dx > WIDTH || x + dx < 0) { dx = -dx; }
+        if (y + dy < 0) { dy = -dy; }
+        else if (y + dy > HEIGHT) {
+            if (x > paddlex && x < paddlex + paddlew) {// If it's inside the paddle we change direction
+                dy = -dy;
+            }
+            else {// If its outside the bar, we stop the animation and end the game
+                clearInterval(game_loop);
+            }
+        }
 
         x += dx;
         y += dy;
     }
 
-    init();
+    var game_loop = init();
 })
